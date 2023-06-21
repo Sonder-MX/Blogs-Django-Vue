afc2b28b425b5f5e1b398999c24f95e59d641d20
SonderMX <sonder-mx@outlook.com>
2023-06-21 19:22:44 +0800

    optimized scrollbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,4 +14,26 @@
   margin-left: 40px;
   margin-right: 40px;
 }
+
+html::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+  background-color: #f5f5f5;
+}
+
+html::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: #c0c4cc;
+}
+
+html::-webkit-scrollbar-track {
+  border-radius: 4px;
+  background-color: #f5f5f5;
+}
+
+/* firefox scrollbar */
+html {
+  scrollbar-width: thin;
+  scrollbar-color: #c0c4cc #f5f5f5;
+}
 </style>
